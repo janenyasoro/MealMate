29a13903374042152b6bfe42267aefacfb73b95a
janenyasoro <janenyasoro@gmail.com>
2026-05-18 12:32:55 +0300

    Creating an app that allows the user to track their meals

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    base: '/mealmate/',  // ← ADD THIS (matches your repository name)
     test: {
         globals: true,
         environment: 'jsdom',
